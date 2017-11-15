7a0b535f1141cc85a07a35b1c1fb3b4a5f904838
MattyFromCzech <mattyvrba@gmail.com>
2017-11-15 22:58:55 +0100

    renew 2.0

---
+++ ArcLight_ The Fighter/ArcLight_ The Fighter/GameScreens/MenuScreen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcLight__The_Fighter.GameScreens
+{
+    class MenuScreen
+    {
+        public static void LoadContent()
+        {
+
+        }
+    }
+}
