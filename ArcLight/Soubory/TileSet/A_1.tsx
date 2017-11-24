e709d5f15e3a198b77f69645d536649bd5af4d0f
MattyFromCzech <mattyvrba@gmail.com>
2017-11-24 21:29:13 +0100

    MAP WIP + TileSet

--- ArcLight/Soubory/TileSet/A_1.tsx
+++ ArcLight/Soubory/TileSet/A_1.tsx
@@ -1,4 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TileMap1" tilewidth="32" tileheight="32" tilecount="400" columns="20">
- <image source="../TileMap.png" width="640" height="640"/>
+<tileset name="DungeonTiles" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="TileTextureA.png" trans="72ff00" width="640" height="640"/>
+ <terraintypes>
+  <terrain name="BrickWall" tile="341"/>
+  <terrain name="Brick" tile="341"/>
+ </terraintypes>
+ <tile id="340" terrain=",,,0"/>
+ <tile id="341" terrain=",,0,0"/>
+ <tile id="342" terrain=",,0,"/>
+ <tile id="343" terrain=",,,1"/>
+ <tile id="344" terrain=",,1,1"/>
+ <tile id="345" terrain=",,1,"/>
+ <tile id="346" terrain="0,0,0,"/>
+ <tile id="347" terrain="0,0,,"/>
+ <tile id="348" terrain="0,0,,0"/>
+ <tile id="360" terrain=",0,,0"/>
+ <tile id="361" terrain="0,0,0,0"/>
+ <tile id="362" terrain="0,,0,">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="363" terrain=",1,,1"/>
+ <tile id="364" terrain="1,1,1,1"/>
+ <tile id="365" terrain="1,,1,"/>
+ <tile id="366" terrain="0,,0,"/>
+ <tile id="368" terrain=",0,,0"/>
+ <tile id="380" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="8" x="21.6364" y="6.45455" width="0.181818" height="1.45455"/>
+  </objectgroup>
+ </tile>
+ <tile id="381" terrain="0,0,,"/>
+ <tile id="382" terrain="0,,,"/>
+ <tile id="383" terrain=",1,,"/>
+ <tile id="384" terrain="1,1,,"/>
+ <tile id="385" terrain="1,,,"/>
+ <tile id="386" terrain="0,,0,0"/>
+ <tile id="387" terrain=",,0,0"/>
+ <tile id="388" terrain=",0,0,0"/>
 </tileset>
